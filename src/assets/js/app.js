43d10866f4c81496cab01d8fb851b094411c2e9a
Jimut <jemdhali67@gmail.com>
2016-05-08 00:46:15 +0530

    Basic boilerplate

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,7 +1,12 @@
 (function(){
-  var app = angular.module('Gallery', []);
+  var app = angular.module('Gallery', ['ngMaterial']);
 
-  app.controller('GalleryController', function(){
-    
+  app.controller('GalleryController', function($scope){
+    $scope.images = [
+      'assets/img/img-1.jpg',
+      'assets/img/img-2.jpg',
+      'assets/img/img-3.jpg',
+      'assets/img/img-4.jpg'
+    ];
   });
 })();
